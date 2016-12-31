07f2e47ec7d26da40ebd968ad6e875b90f2cc3dc
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2016-12-31 11:28:14 +0100

    Fix jump left/right

--- src/engine/lara/statehandler_26.h
+++ src/engine/lara/statehandler_26.h
@@ -19,6 +19,8 @@ namespace engine
             {
                 if( getFallSpeed() > core::FreeFallSpeedThreshold )
                     setTargetState(LaraStateId::FreeFall);
+                else
+                    setTargetState(LaraStateId::JumpLeft);
 
                 return {};
             }
