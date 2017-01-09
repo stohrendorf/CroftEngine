dfee8a73d9f0980296212a1cbdd7971378866dbf
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2017-01-09 01:41:19 +0100

    Formatting

--- src/audio/utils.cpp
+++ src/audio/utils.cpp
@@ -4,20 +4,20 @@
 
 namespace audio
 {
-// ======== Audio source global methods ========
-
-bool checkALError(const char* func, int line)
-{
-    ALenum err = alGetError();
-    if(err != AL_NO_ERROR)
+    bool checkALError(const char* func, int line)
     {
-        const char* errStr = alGetString(err);
-        if(errStr == nullptr)
-            errStr = "<unknown>";
+        ALenum err = alGetError();
+        if( err != AL_NO_ERROR )
+        {
+            const char* errStr = alGetString(err);
+            if( errStr == nullptr )
+            {
+                errStr = "<unknown>";
+            }
 
-        BOOST_LOG_TRIVIAL(warning) << "OpenAL error (in " << func << ":" << line << "): " << errStr;
-        return true;
+            BOOST_LOG_TRIVIAL(warning) << "OpenAL error 0x" << std::hex << err << std::dec << " (in " << func << ":" << line << "): " << errStr;
+            return true;
+        }
+        return false;
     }
-    return false;
-}
 } // namespace audio
--- src/audio/utils.h
+++ src/audio/utils.h
@@ -4,16 +4,13 @@
 
 namespace audio
 {
-// Error handling routines.
-
-bool checkALError(const char* func, int line);    // AL-specific error handler.
+    bool checkALError(const char* func, int line);    // AL-specific error handler.
 
 #define CHECK_AL_ERROR() checkALError(BOOST_CURRENT_FUNCTION, __LINE__)
 
 #ifndef NDEBUG
-#define DEBUG_CHECK_AL_ERROR() CHECK_AL_ERROR()
+    #define DEBUG_CHECK_AL_ERROR() CHECK_AL_ERROR()
 #else
-#define DEBUG_CHECK_AL_ERROR() false
+    #define DEBUG_CHECK_AL_ERROR() false
 #endif
-
 } // namespace audio
