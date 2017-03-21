0f9a7abe4ed3cb929f5e664550ad7a2c8ef409a5
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2017-03-21 02:04:15 +0100

    Fix enum generation, use controller layout abstraction

---
+++ src/engine/controllerlayout.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "controlleraxis.h"
+#include "controllerbutton.h"
+
+#include <yaml-cpp/yaml.h>
+#include <GLFW/glfw3.h>
+
+#include <map>
+#include <set>
+
+namespace engine
+{
+class ControllerLayout
+{
+public:
+    explicit ControllerLayout(const std::string& configFilename)
+    {
+        YAML::Node config = YAML::LoadFile(configFilename);
+        for(const auto& name : config["names"])
+        {
+            m_names.insert(name.as<std::string>());
+        }
+        for(const auto& button : config["buttons"])
+        {
+            m_buttons[EnumUtil<ControllerButton>::fromString(button["name"].as<std::string>())] = button["index"].as<int>();
+        }
+        for(const auto& axis : config["axes"])
+        {
+            m_axes[EnumUtil<ControllerAxis>::fromString(axis["name"].as<std::string>())] = axis["index"].as<int>();
+        }
+    }
+
+    bool isButtonPressed(ControllerButton btn, const gsl::span<const uint8_t>& buttons) const
+    {
+        const auto it = m_buttons.find(btn);
+        if(it == m_buttons.end())
+            return false;
+
+        return buttons.size() > it->second && buttons[it->second] == GLFW_PRESS;
+    };
+
+    double getAxisValue(ControllerAxis axis, const gsl::span<const float>& values, float def) const
+    {
+        const auto it = m_axes.find(axis);
+        if(it == m_axes.end())
+            return false;
+
+        return values.size() > it->second ? values[it->second] : def;
+    };
+
+    const std::set<std::string>& getNames() const
+    {
+        return m_names;
+    }
+
+private:
+    std::set<std::string> m_names;
+    std::map<ControllerAxis, int> m_axes;
+    std::map<ControllerButton, int> m_buttons;
+};
+}
--- src/engine/inputhandler.h
+++ src/engine/inputhandler.h
@@ -2,8 +2,7 @@
 
 #include "inputstate.h"
 
-#include "controlleraxis.h"
-#include "controllerbutton.h"
+#include "controllerlayout.h"
 
 #include <gsl/gsl>
 #include <GLFW/glfw3.h>
@@ -15,7 +14,8 @@ namespace engine
     {
     public:
         explicit InputHandler(const gsl::not_null<GLFWwindow*>& window)
-            : m_window(window)
+            : m_window{ window }
+            , m_controllerLayout{ "3rdparty/controller-data/Xbox-14-6-Windows-1.json" }
         {
             glfwGetCursorPos(m_window, &m_lastCursorX, &m_lastCursorY);
 
@@ -28,10 +28,10 @@ namespace engine
                 if( name == nullptr )
                     continue;
 
-                if( std::strcmp(name, "Xbox 360 Controller") != 0 && std::strcmp(name, "Wireless Xbox 360 Controller") != 0 )
+                if(m_controllerLayout.getNames().find(name) == m_controllerLayout.getNames().end())
                     continue;
 
-                m_xboxController = i;
+                m_controllerIndex = i;
                 break;
             }
         }
@@ -39,101 +39,64 @@ namespace engine
 
         void update()
         {
-            // axes
-            static const constexpr int LS_X = 0;
-            static const constexpr int LS_Y = 1;
-            static const constexpr int RS_X = 2;
-            static const constexpr int RS_Y = 3;
-            static const constexpr int LTRIGGER = 4;
-            static const constexpr int RTRIGGER = 5;
-
-            // buttons
-            static const constexpr int A = 0;
-            static const constexpr int B = 1;
-            static const constexpr int X = 2;
-            static const constexpr int Y = 3;
-            static const constexpr int LB = 4;
-            static const constexpr int RB = 5;
-            static const constexpr int BACK = 6;
-            static const constexpr int START = 7;
-            static const constexpr int LS = 8;
-            static const constexpr int RS = 9;
-            static const constexpr int DPAD_UP = 10;
-            static const constexpr int DPAD_RIGHT = 11;
-            static const constexpr int DPAD_DOWN = 12;
-            static const constexpr int DPAD_LEFT = 13;
-
             static const constexpr float AxisThreshold = 0.5f;
 
-            int xboxButtonCount = 0;
-            const uint8_t* xboxButtons = nullptr;
-            int xboxAxisCount = 0;
-            const float* xboxAxisValues = nullptr;
+            int controllerButtonCount = 0;
+            const uint8_t* controllerButtons = nullptr;
+            int controllerAxisCount = 0;
+            const float* controllerAxisValues = nullptr;
 
-            auto isXboxButtonPressed = [&xboxButtonCount, &xboxButtons](int idx)
+            if(m_controllerIndex >= 0)
             {
-                return xboxButtonCount > idx && xboxButtons != nullptr && xboxButtons[idx] == GLFW_PRESS;
-            };
-
-            auto getXboxAxis = [&xboxAxisCount, &xboxAxisValues](int idx, float def) -> float
-            {
-                if(xboxAxisCount <= idx || xboxAxisValues == nullptr)
-                    return def;
-
-                return xboxAxisValues[idx];
-            };
-
-            if(m_xboxController >= 0)
-            {
-                xboxButtons = glfwGetJoystickButtons(m_xboxController, &xboxButtonCount);
-                xboxAxisValues = glfwGetJoystickAxes(m_xboxController, &xboxAxisCount);
+                controllerButtons = glfwGetJoystickButtons(m_controllerIndex, &controllerButtonCount);
+                controllerAxisValues = glfwGetJoystickAxes(m_controllerIndex, &controllerAxisCount);
             }
 
             auto left = glfwGetKey(m_window, GLFW_KEY_A) == GLFW_PRESS;
-            if(isXboxButtonPressed(DPAD_LEFT))
+            if(m_controllerLayout.isButtonPressed(ControllerButton::DPAD_LEFT, gsl::span<const uint8_t>{controllerButtons, controllerButtonCount}))
                 left = true;
 
             auto right = glfwGetKey(m_window, GLFW_KEY_D) == GLFW_PRESS;
-            if(isXboxButtonPressed(DPAD_RIGHT))
+            if(m_controllerLayout.isButtonPressed(ControllerButton::DPAD_RIGHT, gsl::span<const uint8_t>{controllerButtons, controllerButtonCount}))
                 right = true;
 
             auto stepLeft = glfwGetKey(m_window, GLFW_KEY_Q) == GLFW_PRESS;
-            if(isXboxButtonPressed(LB))
+            if(m_controllerLayout.isButtonPressed(ControllerButton::LB, gsl::span<const uint8_t>{controllerButtons, controllerButtonCount}))
                 stepLeft = true;
 
             auto stepRight = glfwGetKey(m_window, GLFW_KEY_E) == GLFW_PRESS;
-            if(isXboxButtonPressed(RB))
+            if(m_controllerLayout.isButtonPressed(ControllerButton::RB, gsl::span<const uint8_t>{controllerButtons, controllerButtonCount}))
                 stepRight = true;
 
             auto forward = glfwGetKey(m_window, GLFW_KEY_W) == GLFW_PRESS;
-            if(isXboxButtonPressed(DPAD_UP))
+            if(m_controllerLayout.isButtonPressed(ControllerButton::DPAD_UP, gsl::span<const uint8_t>{controllerButtons, controllerButtonCount}))
                 forward = true;
 
             auto backward = glfwGetKey(m_window, GLFW_KEY_S) == GLFW_PRESS;
-            if(isXboxButtonPressed(DPAD_DOWN))
+            if(m_controllerLayout.isButtonPressed(ControllerButton::DPAD_DOWN, gsl::span<const uint8_t>{controllerButtons, controllerButtonCount}))
                 backward = true;
 
             m_inputState.moveSlow = glfwGetKey(m_window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS
                                     || glfwGetKey(m_window, GLFW_KEY_RIGHT_SHIFT) == GLFW_PRESS;
-            if(isXboxButtonPressed(B))
+            if(m_controllerLayout.isButtonPressed(ControllerButton::B, gsl::span<const uint8_t>{controllerButtons, controllerButtonCount}))
                 m_inputState.moveSlow = true;
 
             m_inputState.action = glfwGetKey(m_window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS
                                   || glfwGetKey(m_window, GLFW_KEY_RIGHT_CONTROL) == GLFW_PRESS
                                   || glfwGetMouseButton(m_window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS;
-            if(isXboxButtonPressed(X))
+            if(m_controllerLayout.isButtonPressed(ControllerButton::X, gsl::span<const uint8_t>{controllerButtons, controllerButtonCount}))
                 m_inputState.action = true;
 
             m_inputState.jump = glfwGetKey(m_window, GLFW_KEY_SPACE) == GLFW_PRESS;
-            if(isXboxButtonPressed(A))
+            if(m_controllerLayout.isButtonPressed(ControllerButton::A, gsl::span<const uint8_t>{controllerButtons, controllerButtonCount}))
                 m_inputState.jump = true;
 
             m_inputState.roll = glfwGetKey(m_window, GLFW_KEY_X) == GLFW_PRESS;
-            if(isXboxButtonPressed(BACK))
+            if(m_controllerLayout.isButtonPressed(ControllerButton::BACK, gsl::span<const uint8_t>{controllerButtons, controllerButtonCount}))
                 m_inputState.roll = true;
 
             m_inputState.freeLook = glfwGetMouseButton(m_window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS;
-            if(isXboxButtonPressed(Y))
+            if(m_controllerLayout.isButtonPressed(ControllerButton::Y, gsl::span<const uint8_t>{controllerButtons, controllerButtonCount}))
                 m_inputState.freeLook = true;
 
             double x, y;
@@ -141,18 +104,18 @@ namespace engine
 
             if(m_inputState.freeLook)
             {
-                x += getXboxAxis(LS_X, 0) * 500;
-                y += getXboxAxis(LS_Y, 0) * 500;
+                x += m_controllerLayout.getAxisValue(ControllerAxis::LS_X, gsl::span<const float>{controllerAxisValues, controllerAxisCount}, 0) * 500;
+                y += m_controllerLayout.getAxisValue(ControllerAxis::LS_Y, gsl::span<const float>{controllerAxisValues, controllerAxisCount}, 0) * 500;
             }
             else
             {
-                if(getXboxAxis(LS_Y, 0) > AxisThreshold)
+                if(m_controllerLayout.getAxisValue(ControllerAxis::LS_Y, gsl::span<const float>{controllerAxisValues, controllerAxisCount}, 0) > AxisThreshold)
                     forward = true;
-                if(getXboxAxis(LS_Y, 0) < -AxisThreshold)
+                if(m_controllerLayout.getAxisValue(ControllerAxis::LS_Y, gsl::span<const float>{controllerAxisValues, controllerAxisCount}, 0) < -AxisThreshold)
                     backward = true;
-                if(getXboxAxis(LS_X, 0) < -AxisThreshold)
+                if(m_controllerLayout.getAxisValue(ControllerAxis::LS_X, gsl::span<const float>{controllerAxisValues, controllerAxisCount}, 0) < -AxisThreshold)
                     left = true;
-                if(getXboxAxis(LS_X, 0) > AxisThreshold)
+                if(m_controllerLayout.getAxisValue(ControllerAxis::LS_X, gsl::span<const float>{controllerAxisValues, controllerAxisCount}, 0) > AxisThreshold)
                     right = true;
             }
 
@@ -179,6 +142,7 @@ namespace engine
         double m_lastCursorX = 0;
         double m_lastCursorY = 0;
 
-        int m_xboxController = -1;
+        int m_controllerIndex = -1;
+        ControllerLayout m_controllerLayout;
     };
 }
