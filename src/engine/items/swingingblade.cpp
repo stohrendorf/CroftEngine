65040e088b9801cc7cee34186741c116bc779db5
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2018-09-01 12:42:27 +0200

    Fix missing blood for swinging blades

--- src/engine/items/swingingblade.cpp
+++ src/engine/items/swingingblade.cpp
@@ -1,6 +1,7 @@
 #include "swingingblade.h"
 
 #include "engine/heightinfo.h"
+#include "engine/laranode.h"
 #include "level/level.h"
 
 namespace engine
@@ -9,16 +10,36 @@ namespace items
 {
 void SwingingBlade::update()
 {
-    if( m_state.updateActivationTimeout() )
+    if( !m_state.updateActivationTimeout() )
+    {
+        if( m_state.current_anim_state == 2 )
+        {
+            m_state.goal_anim_state = 0;
+        }
+    }
+    else
     {
         if( m_state.current_anim_state == 0 )
         {
             m_state.goal_anim_state = 2;
         }
     }
-    else if( m_state.current_anim_state == 2 )
+
+    if( m_state.current_anim_state == 2 && m_state.touch_bits != 0 )
     {
-        m_state.goal_anim_state = 0;
+        getLevel().m_lara->m_state.is_hit = true;
+        getLevel().m_lara->m_state.health -= 100;
+
+        core::TRCoordinates splatPos{
+                getLevel().m_lara->m_state.position.position.X + util::rand15s() / 256,
+                getLevel().m_lara->m_state.position.position.Y - util::rand15() / 44,
+                getLevel().m_lara->m_state.position.position.Z + util::rand15s() / 256
+        };
+        auto fx = engine::createBloodSplat( getLevel(),
+                                            core::RoomBoundPosition{m_state.position.room, splatPos},
+                                            getLevel().m_lara->m_state.speed,
+                                            getLevel().m_lara->m_state.rotation.Y + util::rand15s( +22_deg ) );
+        getLevel().m_particles.emplace_back( fx );
     }
 
     auto room = m_state.position.room;
