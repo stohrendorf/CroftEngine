eec5b90a8e3b5cf4931846a5945f671f7e6df9a4
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2018-09-10 00:42:24 +0200

    De-copy/paste cutscene actors

--- src/engine/items/cutsceneactors.cpp
+++ src/engine/items/cutsceneactors.cpp
@@ -6,13 +6,27 @@ namespace engine
 {
 namespace items
 {
-CutsceneActor1::CutsceneActor1(const gsl::not_null<level::Level*>& level, const std::string& name,
-                               const gsl::not_null<const loader::Room*>& room, const loader::Item& item,
-                               const loader::SkeletalModelType& animatedModel)
+CutsceneActor::CutsceneActor(const gsl::not_null<level::Level*>& level, const std::string& name,
+                             const gsl::not_null<const loader::Room*>& room, const loader::Item& item,
+                             const loader::SkeletalModelType& animatedModel)
         : ModelItemNode( level, name, room, item, true, animatedModel )
 {
     activate();
     m_state.rotation.Y = 0_deg;
+}
+
+void CutsceneActor::update()
+{
+    m_state.rotation.Y = getLevel().m_cameraController->getTargetRotation().Y;
+    m_state.position.position = getLevel().m_cameraController->getTRPosition().position;
+    ModelItemNode::update();
+}
+
+CutsceneActor1::CutsceneActor1(const gsl::not_null<level::Level*>& level, const std::string& name,
+                               const gsl::not_null<const loader::Room*>& room, const loader::Item& item,
+                               const loader::SkeletalModelType& animatedModel)
+        : CutsceneActor( level, name, room, item, animatedModel )
+{
     if( false ) // FIXME cinematic_level == CUTSCENE2 || cinematic_level == CUTSCENE4 )
     {
         const auto& laraPistol = level->findAnimatedModelForType( engine::TR1ItemId::LaraPistolsAnim );
@@ -21,57 +35,9 @@ CutsceneActor1::CutsceneActor1(const gsl::not_null<level::Level*>& level, const 
     }
 }
 
-CutsceneActor2::CutsceneActor2(const gsl::not_null<level::Level*>& level, const std::string& name,
-                               const gsl::not_null<const loader::Room*>& room, const loader::Item& item,
-                               const loader::SkeletalModelType& animatedModel)
-        : ModelItemNode( level, name, room, item, true, animatedModel )
-{
-    activate();
-    m_state.rotation.Y = 0_deg;
-}
-
-CutsceneActor3::CutsceneActor3(const gsl::not_null<level::Level*>& level, const std::string& name,
-                               const gsl::not_null<const loader::Room*>& room, const loader::Item& item,
-                               const loader::SkeletalModelType& animatedModel)
-        : ModelItemNode( level, name, room, item, true, animatedModel )
-{
-    activate();
-    m_state.rotation.Y = 0_deg;
-}
-
-CutsceneActor4::CutsceneActor4(const gsl::not_null<level::Level*>& level, const std::string& name,
-                               const gsl::not_null<const loader::Room*>& room, const loader::Item& item,
-                               const loader::SkeletalModelType& animatedModel)
-        : ModelItemNode( level, name, room, item, true, animatedModel )
-{
-    activate();
-    m_state.rotation.Y = 0_deg;
-}
-
-void CutsceneActor1::update()
-{
-    m_state.rotation.Y = getLevel().m_cameraController->getTargetRotation().Y;
-    m_state.position.position = getLevel().m_cameraController->getTRPosition().position;
-    ModelItemNode::update();
-}
-
-void CutsceneActor2::update()
-{
-    m_state.rotation.Y = getLevel().m_cameraController->getTargetRotation().Y;
-    m_state.position.position = getLevel().m_cameraController->getTRPosition().position;
-    ModelItemNode::update();
-}
-
-void CutsceneActor3::update()
-{
-    m_state.rotation.Y = getLevel().m_cameraController->getTargetRotation().Y;
-    m_state.position.position = getLevel().m_cameraController->getTRPosition().position;
-    ModelItemNode::update();
-}
-
 void CutsceneActor4::update()
 {
-    ModelItemNode::update();
+    ModelItemNode::update(); // NOLINT(bugprone-parent-virtual-call)
 }
 }
 }
--- src/engine/items/cutsceneactors.h
+++ src/engine/items/cutsceneactors.h
@@ -6,7 +6,20 @@ namespace engine
 {
 namespace items
 {
-class CutsceneActor1 final : public ModelItemNode
+class CutsceneActor : public ModelItemNode
+{
+public:
+    CutsceneActor(const gsl::not_null<level::Level*>& level,
+                  const std::string& name,
+                  const gsl::not_null<const loader::Room*>& room,
+                  const loader::Item& item,
+                  const loader::SkeletalModelType& animatedModel);
+
+    void update() override;
+};
+
+
+class CutsceneActor1 final : public CutsceneActor
 {
 public:
     CutsceneActor1(const gsl::not_null<level::Level*>& level,
@@ -14,45 +27,46 @@ public:
                    const gsl::not_null<const loader::Room*>& room,
                    const loader::Item& item,
                    const loader::SkeletalModelType& animatedModel);
-
-    void update() override;
 };
 
 
-class CutsceneActor2 final : public ModelItemNode
+class CutsceneActor2 final : public CutsceneActor
 {
 public:
     CutsceneActor2(const gsl::not_null<level::Level*>& level,
                    const std::string& name,
                    const gsl::not_null<const loader::Room*>& room,
                    const loader::Item& item,
-                   const loader::SkeletalModelType& animatedModel);
-
-    void update() override;
+                   const loader::SkeletalModelType& animatedModel)
+            : CutsceneActor( level, name, room, item, animatedModel )
+    {}
 };
 
 
-class CutsceneActor3 final : public ModelItemNode
+class CutsceneActor3 final : public CutsceneActor
 {
 public:
     CutsceneActor3(const gsl::not_null<level::Level*>& level,
                    const std::string& name,
                    const gsl::not_null<const loader::Room*>& room,
                    const loader::Item& item,
-                   const loader::SkeletalModelType& animatedModel);
-
-    void update() override;
+                   const loader::SkeletalModelType& animatedModel)
+            : CutsceneActor( level, name, room, item, animatedModel )
+    {}
 };
 
 
-class CutsceneActor4 final : public ModelItemNode
+class CutsceneActor4 final : public CutsceneActor
 {
+public:
 public:
     CutsceneActor4(const gsl::not_null<level::Level*>& level,
                    const std::string& name,
                    const gsl::not_null<const loader::Room*>& room,
                    const loader::Item& item,
-                   const loader::SkeletalModelType& animatedModel);
+                   const loader::SkeletalModelType& animatedModel)
+            : CutsceneActor( level, name, room, item, animatedModel )
+    {}
 
     void update() override;
 };
