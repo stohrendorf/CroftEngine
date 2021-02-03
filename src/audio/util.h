312848427bf2c7cc02c71c70bc8f0fe2685bda75
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2021-02-03 19:25:48 +0100

    fix unaligned data read

--- src/audio/util.h
+++ src/audio/util.h
@@ -13,8 +13,10 @@ inline std::shared_ptr<SoLoud::Wav> loadWav(const uint8_t* data)
   Expects(data[0] == 'R' && data[1] == 'I' && data[2] == 'F' && data[3] == 'F');
   Expects(data[8] == 'W' && data[9] == 'A' && data[10] == 'V' && data[11] == 'E');
 
-  // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
-  sndfile::MemBufferFileIo wavMem(data, static_cast<size_t>(*reinterpret_cast<const uint32_t*>(data + 4)) + 8u);
+  // this is an unaligned read
+  uint32_t dataSize = 0;
+  std::memcpy(&dataSize, data + 4, sizeof(uint32_t));
+  sndfile::MemBufferFileIo wavMem(data, static_cast<size_t>(dataSize) + 8u);
   SF_INFO sfInfo;
   memset(&sfInfo, 0, sizeof(sfInfo));
   SNDFILE* sfFile = sf_open_virtual(&wavMem, SFM_READ, &sfInfo, &wavMem);
