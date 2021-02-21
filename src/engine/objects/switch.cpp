209fa7f802acef2797277216348c767a528e5e7c
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2021-02-21 07:32:51 +0100

    close #139

--- src/engine/objects/switch.cpp
+++ src/engine/objects/switch.cpp
@@ -9,7 +9,7 @@ namespace engine::objects
 {
 void Switch::collide(CollisionInfo& /*collisionInfo*/)
 {
-  if(!getWorld().getPresenter().getInputHandler().hasDebouncedAction(hid::Action::Action))
+  if(!getWorld().getPresenter().getInputHandler().hasAction(hid::Action::Action))
     return;
 
   if(getWorld().getObjectManager().getLara().getHandStatus() != HandStatus::None)
