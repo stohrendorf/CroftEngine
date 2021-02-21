d3cf01914988bb761d305097f56091a5d4f74d88
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2021-02-21 07:27:43 +0100

    minor code cleanups

--- src/engine/inventory.cpp
+++ src/engine/inventory.cpp
@@ -15,9 +15,9 @@ void Inventory::put(const core::TypeId id, const size_t quantity)
 
   switch(id.get_as<TR1ItemId>())
   {
-  case TR1ItemId::PistolsSprite:
+  case TR1ItemId::PistolsSprite: [[fallthrough]];
   case TR1ItemId::Pistols: m_inventory[TR1ItemId::Pistols] = 1; break;
-  case TR1ItemId::ShotgunSprite:
+  case TR1ItemId::ShotgunSprite: [[fallthrough]];
   case TR1ItemId::Shotgun:
     if(const auto clips = count(TR1ItemId::ShotgunAmmoSprite))
     {
@@ -28,7 +28,7 @@ void Inventory::put(const core::TypeId id, const size_t quantity)
     // TODO replaceItems( ShotgunSprite, ShotgunAmmoSprite );
     m_inventory[TR1ItemId::Shotgun] = 1;
     break;
-  case TR1ItemId::MagnumsSprite:
+  case TR1ItemId::MagnumsSprite: [[fallthrough]];
   case TR1ItemId::Magnums:
     if(const auto clips = count(TR1ItemId::MagnumAmmoSprite))
     {
@@ -39,7 +39,7 @@ void Inventory::put(const core::TypeId id, const size_t quantity)
     // TODO replaceItems( MagnumsSprite, MagnumAmmoSprite );
     m_inventory[TR1ItemId::Magnums] = 1;
     break;
-  case TR1ItemId::UzisSprite:
+  case TR1ItemId::UzisSprite: [[fallthrough]];
   case TR1ItemId::Uzis:
     if(const auto clips = count(TR1ItemId::UziAmmoSprite))
     {
@@ -50,55 +50,55 @@ void Inventory::put(const core::TypeId id, const size_t quantity)
     // TODO replaceItems( UzisSprite, UziAmmoSprite );
     m_inventory[TR1ItemId::Uzis] = 1;
     break;
-  case TR1ItemId::ShotgunAmmoSprite:
+  case TR1ItemId::ShotgunAmmoSprite: [[fallthrough]];
   case TR1ItemId::ShotgunAmmo:
     if(count(TR1ItemId::ShotgunSprite) > 0)
       m_shotgunAmmo.ammo += 12u;
     else
       m_inventory[TR1ItemId::ShotgunAmmo] += quantity;
     break;
-  case TR1ItemId::MagnumAmmoSprite:
+  case TR1ItemId::MagnumAmmoSprite: [[fallthrough]];
   case TR1ItemId::MagnumAmmo:
     if(count(TR1ItemId::MagnumsSprite) > 0)
       m_magnumsAmmo.ammo += 50u;
     else
       m_inventory[TR1ItemId::MagnumAmmo] += quantity;
     break;
-  case TR1ItemId::UziAmmoSprite:
+  case TR1ItemId::UziAmmoSprite: [[fallthrough]];
   case TR1ItemId::UziAmmo:
     if(count(TR1ItemId::UzisSprite) > 0)
       m_uzisAmmo.ammo += 100u;
     else
       m_inventory[TR1ItemId::UziAmmo] += quantity;
     break;
-  case TR1ItemId::SmallMedipackSprite:
+  case TR1ItemId::SmallMedipackSprite: [[fallthrough]];
   case TR1ItemId::SmallMedipack: m_inventory[TR1ItemId::SmallMedipack] += quantity; break;
-  case TR1ItemId::LargeMedipackSprite:
+  case TR1ItemId::LargeMedipackSprite: [[fallthrough]];
   case TR1ItemId::LargeMedipack: m_inventory[TR1ItemId::LargeMedipack] += quantity; break;
-  case TR1ItemId::Puzzle1Sprite:
+  case TR1ItemId::Puzzle1Sprite: [[fallthrough]];
   case TR1ItemId::Puzzle1: m_inventory[TR1ItemId::Puzzle1] += quantity; break;
-  case TR1ItemId::Puzzle2Sprite:
+  case TR1ItemId::Puzzle2Sprite: [[fallthrough]];
   case TR1ItemId::Puzzle2: m_inventory[TR1ItemId::Puzzle2] += quantity; break;
-  case TR1ItemId::Puzzle3Sprite:
+  case TR1ItemId::Puzzle3Sprite: [[fallthrough]];
   case TR1ItemId::Puzzle3: m_inventory[TR1ItemId::Puzzle3] += quantity; break;
-  case TR1ItemId::Puzzle4Sprite:
+  case TR1ItemId::Puzzle4Sprite: [[fallthrough]];
   case TR1ItemId::Puzzle4: m_inventory[TR1ItemId::Puzzle4] += quantity; break;
-  case TR1ItemId::LeadBarSprite:
+  case TR1ItemId::LeadBarSprite: [[fallthrough]];
   case TR1ItemId::LeadBar: m_inventory[TR1ItemId::LeadBar] += quantity; break;
-  case TR1ItemId::Key1Sprite:
+  case TR1ItemId::Key1Sprite: [[fallthrough]];
   case TR1ItemId::Key1: m_inventory[TR1ItemId::Key1] += quantity; break;
-  case TR1ItemId::Key2Sprite:
+  case TR1ItemId::Key2Sprite: [[fallthrough]];
   case TR1ItemId::Key2: m_inventory[TR1ItemId::Key2] += quantity; break;
-  case TR1ItemId::Key3Sprite:
+  case TR1ItemId::Key3Sprite: [[fallthrough]];
   case TR1ItemId::Key3: m_inventory[TR1ItemId::Key3] += quantity; break;
-  case TR1ItemId::Key4Sprite:
+  case TR1ItemId::Key4Sprite: [[fallthrough]];
   case TR1ItemId::Key4: m_inventory[TR1ItemId::Key4] += quantity; break;
-  case TR1ItemId::Item141:
+  case TR1ItemId::Item141: [[fallthrough]];
   case TR1ItemId::Item148: m_inventory[TR1ItemId::Item148] += quantity; break;
-  case TR1ItemId::Item142:
+  case TR1ItemId::Item142: [[fallthrough]];
   case TR1ItemId::Item149: m_inventory[TR1ItemId::Item149] += quantity; break;
-  case TR1ItemId::ScionPiece1:
-  case TR1ItemId::ScionPiece2:
+  case TR1ItemId::ScionPiece1: [[fallthrough]];
+  case TR1ItemId::ScionPiece2: [[fallthrough]];
   case TR1ItemId::ScionPiece5: m_inventory[TR1ItemId::ScionPiece5] += quantity; break;
   default:
     BOOST_LOG_TRIVIAL(warning) << "Cannot add object " << toString(id.get_as<TR1ItemId>()) << " to inventory";
