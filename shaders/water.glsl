9277e91719a02fbd909d4ca5b27cd6bf46e2358a
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2021-03-18 23:07:55 +0100

    close EE-133

--- shaders/water.glsl
+++ shaders/water.glsl
@@ -1,12 +1,12 @@
 #include "time_uniform.glsl"
 
 const float Frq1 = 12.6;
-const float TimeMult1 = 0.005;
-const float Amplitude1 = 0.00175;
+const float TimeMult1 = 0.002;
+const float Amplitude1 = 0.001;
 
 const float Frq2 = 19.7;
-const float TimeMult2 = 0.002;
-const float Amplitude2 = 0.0125;
+const float TimeMult2 = 0.001;
+const float Amplitude2 = 0.0055;
 
 void do_water_distortion_frq(inout vec2 uv, in float timeMult, in float scale, in float amplitude)
 {
