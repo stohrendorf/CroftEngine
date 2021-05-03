929af9109c427a96e03071bd398860ec42ea6ae4
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2021-05-03 19:35:09 +0200

    disable debug groups in release builds

--- src/soglb/gl/debuggroup.h
+++ src/soglb/gl/debuggroup.h
@@ -5,20 +5,27 @@
 
 #include <gsl/gsl-lite.hpp>
 
+#ifndef SOGLB_NO_DEBUG_GROUPS
+#  define SOGLB_NO_DEBUG_GROUPS NDEBUG
+#endif
+
 namespace gl
 {
+#if SOGLB_NO_DEBUG_GROUPS
+#  define SOGLB_DEBUGGROUP(name)
+#else
 // https://stackoverflow.com/a/35087985
 
 // NOLINTNEXTLINE(bugprone-reserved-identifier)
-#define _SOGLB_PASTE(x, y) x##y
+#  define _SOGLB_PASTE(x, y) x##y
 // NOLINTNEXTLINE(bugprone-reserved-identifier)
-#define _SOGLB_CAT(x, y) _SOGLB_PASTE(x, y)
+#  define _SOGLB_CAT(x, y) _SOGLB_PASTE(x, y)
 
-#define SOGLB_DEBUGGROUP(name)                                                \
-  [[maybe_unused]] ::gl::DebugGroup _SOGLB_CAT(_soglb_debug_group_, __LINE__) \
-  {                                                                           \
-    name                                                                      \
-  }
+#  define SOGLB_DEBUGGROUP(name)                                                \
+    [[maybe_unused]] ::gl::DebugGroup _SOGLB_CAT(_soglb_debug_group_, __LINE__) \
+    {                                                                           \
+      name                                                                      \
+    }
 
 class DebugGroup final
 {
@@ -32,11 +39,8 @@ public:
   }
 
   DebugGroup(const DebugGroup&) = delete;
-
   DebugGroup(DebugGroup&&) noexcept = delete;
-
   DebugGroup& operator=(const DebugGroup&) = delete;
-
   DebugGroup& operator=(DebugGroup&&) = delete;
 
   ~DebugGroup()
@@ -44,4 +48,5 @@ public:
     GL_ASSERT(api::popDebugGroup());
   }
 };
+#endif
 } // namespace gl
