57ce1d83d94f28e3cd04d371d6c3720d2752f522
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2021-05-19 20:05:33 +0200

    simplify frame/heading styling

--- src/ui/util.cpp
+++ src/ui/util.cpp
@@ -8,7 +8,6 @@ std::unique_ptr<Label> createFrame(const glm::ivec2& position, const glm::ivec2&
 {
   auto result = std::make_unique<Label>(position, " ");
   result->bgndSize = size;
-  result->backgroundAlpha = 240;
   result->backgroundGouraud = Label::makeBackgroundCircle(gl::SRGBA8{0, 128, 0, 255}, gl::SRGBA8{0, 0, 0, 255});
   result->backgroundGouraudAlpha = 192;
   result->outlineAlpha = 255;
@@ -20,8 +19,6 @@ std::unique_ptr<Label> createHeading(const std::string& heading, const glm::ivec
   auto result = std::make_unique<Label>(position, heading);
   result->bgndSize = size;
   result->backgroundAlpha = 255;
-  result->backgroundGouraud = Label::makeBackgroundCircle(gl::SRGB8{0, 0, 0}, 255, 255);
-  result->backgroundGouraudAlpha = 255;
   result->outlineAlpha = 255;
   return result;
 }
