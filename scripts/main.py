a0e7f42d8d3a27302fc988e9d51ad2823937bb87
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2021-08-12 23:29:28 +0200

    add more cheat items

--- scripts/main.py
+++ scripts/main.py
@@ -18,6 +18,9 @@ if False:
             TR1ItemId.Puzzle2: 10,
             TR1ItemId.Puzzle3: 10,
             TR1ItemId.Puzzle4: 10,
+            TR1ItemId.UzisSprite: 1,
+            TR1ItemId.MagnumsSprite: 1,
+            TR1ItemId.ShotgunSprite: 1,
         }
     }
 else:
