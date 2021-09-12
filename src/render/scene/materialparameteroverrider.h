8cd37c3b6f3699a4fc427faa0eecd77e2f1b90e0
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2021-09-12 11:38:26 +0200

    refactor material parameter overriding

--- src/render/scene/materialparameteroverrider.h
+++ src/render/scene/materialparameteroverrider.h
@@ -7,71 +7,89 @@
 
 namespace render::scene
 {
+template<typename T>
+class SingleMaterialParameterOverrider final
+{
+public:
+  [[nodiscard]] const std::function<T>* find(const std::string& name) const
+  {
+    const auto it = m_setters.find(name);
+    if(it != m_setters.end())
+      return &it->second;
+
+    return nullptr;
+  }
+
+  void bind(const std::string& name, const std::function<T>& setter)
+  {
+    m_setters[name] = setter;
+  }
+
+  void bind(const std::string& name, std::function<T>&& setter)
+  {
+    m_setters[name] = std::move(setter);
+  }
+
+private:
+  boost::container::flat_map<std::string, std::function<T>> m_setters;
+};
+
 class MaterialParameterOverrider
 {
 public:
   virtual ~MaterialParameterOverrider() = default;
 
-  const std::function<UniformParameter::UniformValueSetter>* findUniformSetter(const std::string& name) const
+  [[nodiscard]] const std::function<UniformParameter::UniformValueSetter>*
+    findUniformSetter(const std::string& name) const
   {
-    const auto it = m_uniformSetters.find(name);
-    if(it != m_uniformSetters.end())
-      return &it->second;
-
-    return nullptr;
+    return m_uniformSetters.find(name);
   }
 
-  const std::function<UniformBlockParameter::BufferBinder>* findUniformBlockBinder(const std::string& name) const
+  [[nodiscard]] const std::function<UniformBlockParameter::BufferBinder>*
+    findUniformBlockBinder(const std::string& name) const
   {
-    const auto it = m_uniformBlockBinders.find(name);
-    if(it != m_uniformBlockBinders.end())
-      return &it->second;
-
-    return nullptr;
+    return m_uniformBlockBinders.find(name);
   }
 
-  const std::function<BufferParameter::BufferBinder>* findShaderStorageBlockBinder(const std::string& name) const
+  [[nodiscard]] const std::function<BufferParameter::BufferBinder>*
+    findShaderStorageBlockBinder(const std::string& name) const
   {
-    const auto it = m_bufferBinders.find(name);
-    if(it != m_bufferBinders.end())
-      return &it->second;
-
-    return nullptr;
+    return m_bufferBinders.find(name);
   }
 
   void bind(const std::string& name, const std::function<UniformParameter::UniformValueSetter>& setter)
   {
-    m_uniformSetters[name] = setter;
+    m_uniformSetters.bind(name, setter);
   }
 
   void bind(const std::string& name, std::function<UniformParameter::UniformValueSetter>&& setter)
   {
-    m_uniformSetters[name] = std::move(setter);
+    m_uniformSetters.bind(name, std::move(setter));
   }
 
   void bind(const std::string& name, const std::function<BufferParameter::BufferBinder>& binder)
   {
-    m_bufferBinders[name] = binder;
+    m_bufferBinders.bind(name, binder);
   }
 
   void bind(const std::string& name, std::function<BufferParameter::BufferBinder>&& binder)
   {
-    m_bufferBinders[name] = std::move(binder);
+    m_bufferBinders.bind(name, std::move(binder));
   }
 
   void bind(const std::string& name, const std::function<UniformBlockParameter::BufferBinder>& binder)
   {
-    m_uniformBlockBinders[name] = binder;
+    m_uniformBlockBinders.bind(name, binder);
   }
 
   void bind(const std::string& name, std::function<UniformBlockParameter::BufferBinder>&& binder)
   {
-    m_uniformBlockBinders[name] = std::move(binder);
+    m_uniformBlockBinders.bind(name, std::move(binder));
   }
 
 private:
-  boost::container::flat_map<std::string, std::function<UniformParameter::UniformValueSetter>> m_uniformSetters;
-  boost::container::flat_map<std::string, std::function<UniformBlockParameter::BufferBinder>> m_uniformBlockBinders;
-  boost::container::flat_map<std::string, std::function<BufferParameter::BufferBinder>> m_bufferBinders;
+  SingleMaterialParameterOverrider<UniformParameter::UniformValueSetter> m_uniformSetters;
+  SingleMaterialParameterOverrider<UniformBlockParameter::BufferBinder> m_uniformBlockBinders;
+  SingleMaterialParameterOverrider<BufferParameter::BufferBinder> m_bufferBinders;
 };
 } // namespace render::scene
