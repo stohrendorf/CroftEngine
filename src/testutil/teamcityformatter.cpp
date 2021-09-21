1e1ee9a0ee3650d3c4ef0b42e82e41c551855ed9
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2021-09-21 16:42:37 +0200

    fix teamcity test execution

--- src/testutil/teamcityformatter.cpp
+++ src/testutil/teamcityformatter.cpp
@@ -136,8 +136,7 @@ struct TeamcityFormatterRegistrar
   {
     if(underTeamcity())
     {
-      static TeamcityBoostLogFormatter formatter;
-      boost::unit_test::unit_test_log.set_formatter(&formatter);
+      boost::unit_test::unit_test_log.set_formatter(new TeamcityBoostLogFormatter());
       boost::unit_test::unit_test_log.set_threshold_level(boost::unit_test::log_test_units);
     }
   }
