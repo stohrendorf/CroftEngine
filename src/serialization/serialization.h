72dcd8c0ba888b56363069ae897408fd25fac2d1
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2021-11-06 15:29:49 +0100

    bump rapidyaml

--- src/serialization/serialization.h
+++ src/serialization/serialization.h
@@ -336,7 +336,7 @@ public:
     Expects(!tag.empty());
 
     auto normalizedTag
-      = std::string{"!<"}
+      = std::string{"<"}
         + boost::algorithm::replace_all_copy(boost::algorithm::replace_all_copy(tag, ">", "&gt;"), " ", "%20") + ">";
     if(loading)
     {
