74a5d818c487cebe883c5038d9c6fa578c5fdcae
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2022-06-05 16:31:43 +0200

    wrap enums using python script instead of cmake regex mangling

---
+++ cmake/scripts/wrap_enum.py
@@ -0,0 +1,119 @@
+import argparse
+from pathlib import Path
+import re
+from typing import Iterable, Tuple, List
+
+
+def read_enum_txt(name: Path) -> Iterable[Tuple[str, str]]:
+    for line in name.read_text().splitlines():
+        if matches := re.fullmatch(r"(\s*//.+)?", line):
+            pass
+        elif matches := re.fullmatch(r"(.+?)\s*=\s*(.+?)(\s*//.+)?", line):
+            yield matches.group(1), matches.group(2)
+        elif matches := re.fullmatch(r"(.+?)(\s*//.+)?", line):
+            yield matches.group(1), None
+
+
+def generate_header_lines(args, enums: List[Tuple[str, str]]) -> Iterable[str]:
+    yield "#pragma once"
+    yield ""
+    yield "#include <cstdint>"
+    yield "#include <gsl/gsl-lite.hpp>"
+    yield "#include <map>"
+    yield "#include <string>"
+    for include in args.with_includes:
+        yield f"#include {include}"
+    yield ""
+
+    yield f"namespace {args.namespace}"
+    yield "{"
+    yield f"enum class {args.name} : {args.type}"
+    yield "{"
+    for identifier, value in enums:
+        if value is not None:
+            yield f"    {identifier} = {value},"
+        else:
+            yield f"    {identifier},"
+    yield "};"
+
+    yield ""
+    yield f"extern gsl::czstring toString({args.name} val);"
+
+    yield ""
+    yield "template<typename T> struct EnumUtil;"
+
+    yield ""
+    yield f"template<> struct EnumUtil<{args.name}>"
+    yield "{"
+    yield f"    static {args.name} fromString(const std::string& value);"
+    yield f"    static std::map<{args.name}, std::string> all();"
+    yield "    static const char* name()"
+    yield "    {"
+    yield f"        return \"{args.name}\";"
+    yield "    }"
+
+    yield "};"
+
+    yield f"}} // namespace {args.namespace}"
+
+
+def generate_impl_lines(args, enums: List[Tuple[str, str]], include_path: Path) -> Iterable[str]:
+    yield f"#include \"{include_path.as_posix()}\""
+    yield ""
+    yield "#include <cstdint>"
+    yield "#include <gsl/gsl-lite.hpp>"
+    yield "#include <map>"
+    yield "#include <stdexcept>"
+    yield "#include <string>"
+
+    yield f"namespace {args.namespace}"
+    yield "{"
+
+    yield f"gsl::czstring toString({args.name} val)"
+    yield "{"
+    yield "    switch(val)"
+    yield "    {"
+    for identifier, value in enums:
+        yield f"        case {args.name}::{identifier}: return \"{identifier}\";"
+    yield "        default: return nullptr;"
+    yield "    }"
+    yield "}"
+
+    yield f"{args.name} EnumUtil<{args.name}>::fromString(const std::string& value)"
+    yield "{"
+    for identifier, value in enums:
+        yield f"    if(value == \"{identifier}\") return {args.name}::{identifier};"
+    yield f"    throw std::domain_error(\"Value '\" + value + \"' invalid for enum '{args.name}'\");"
+    yield "}"
+
+    yield ""
+    yield f"std::map<{args.name}, std::string> EnumUtil<{args.name}>::all()"
+    yield "{"
+    yield "    return {"
+    for identifier, value in enums:
+        yield f"        {{{args.name}::{identifier}, \"{identifier}\"}},"
+    yield "    };"
+    yield "}"
+    yield f"}} // namespace {args.namespace}"
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--name', type=str)
+    parser.add_argument('--namespace', type=str)
+    parser.add_argument('--type', type=str)
+    parser.add_argument('--source', type=str)
+    parser.add_argument('--dest', type=str)
+    parser.add_argument('--dest-impl', type=str)
+    parser.add_argument('--with-includes', type=str, nargs="*")
+
+    args = parser.parse_args()
+
+    enums = list(read_enum_txt(Path(args.source)))
+    Path(args.dest).write_text("\n".join(generate_header_lines(args, enums)))
+    Path(args.dest_impl).write_text(
+        "\n".join(generate_impl_lines(args, enums, Path(args.dest).relative_to(Path(args.dest_impl).parent))))
+
+
+if __name__ == '__main__':
+    main()
--- cmake/wrap_enum.cmake
+++ cmake/wrap_enum.cmake
@@ -1,3 +1,5 @@
+include( get_python3 )
+
 function( wrap_enum )
     cmake_parse_arguments(
             WRAP_ENUM
@@ -21,172 +23,20 @@ function( wrap_enum )
         return()
     endif()
 
-    message( STATUS "Generating ${WRAP_ENUM_DEST} and ${WRAP_ENUM_DEST_IMPL} from ${WRAP_ENUM_SOURCE}" )
-    file( STRINGS "${WRAP_ENUM_SOURCE}" _vals )
-
-    set( _wrap_enum_tmp ${CMAKE_BINARY_DIR}/wrap-enum.tmp )
-    file( WRITE "${_wrap_enum_tmp}" "" )
-    # headers
-    file( APPEND "${_wrap_enum_tmp}" "#pragma once\n" )
-    file( APPEND "${_wrap_enum_tmp}" "\n" )
-    file( APPEND "${_wrap_enum_tmp}" "#include <cstdint>\n" )
-    file( APPEND "${_wrap_enum_tmp}" "#include <gsl/gsl-lite.hpp>\n" )
-    file( APPEND "${_wrap_enum_tmp}" "#include <map>\n" )
-    file( APPEND "${_wrap_enum_tmp}" "#include <string>\n" )
-    foreach( _include IN LISTS WRAP_ENUM_WITH_INCLUDES )
-        if( NOT ${_include} MATCHES "^<" )
-            set( _include "\"${include}\"" )
-        endif()
-        file( APPEND "${_wrap_enum_tmp}" "#include ${_include}\n" )
-    endforeach()
-    file( APPEND "${_wrap_enum_tmp}" "\n" )
-
-    # Write enum
-    file( APPEND "${_wrap_enum_tmp}" "namespace ${WRAP_ENUM_NAMESPACE}\n" )
-    file( APPEND "${_wrap_enum_tmp}" "{\n" )
-    file( APPEND "${_wrap_enum_tmp}" "enum class ${WRAP_ENUM_NAME} : ${WRAP_ENUM_TYPE}\n" )
-    file( APPEND "${_wrap_enum_tmp}" "{\n" )
-    foreach( _val IN LISTS _vals )
-        # remove trailing whitespace/comments
-        string( REGEX REPLACE "//.*\$" "" _val "${_val}" )
-        string( REPLACE " " "" _val "${_val}" )
-        string( REPLACE "\t" "" _val "${_val}" )
-        if( _val STREQUAL "" )
-            continue()
-        endif()
-
-        # remove "= 123" part
-        string( REGEX REPLACE "=.+\$" "" _ename "${_val}" )
-        if( _ename STREQUAL "" )
-            continue()
-        endif()
-        if( _val MATCHES ".+=.+" )
-            # extract value of "= 123" part
-            string( REGEX REPLACE "^.+=(.+)" "\\1" _eval "${_val}" )
-            # emit: Value = IntValue,
-            file( APPEND "${_wrap_enum_tmp}" "    ${_ename} = ${_eval},\n" )
-        else()
-            # emit: Value,
-            file( APPEND "${_wrap_enum_tmp}" "    ${_ename},\n" )
-        endif()
-    endforeach()
-    file( APPEND "${_wrap_enum_tmp}" "};\n\n" )
-
-    # toString function
-    file( APPEND "${_wrap_enum_tmp}" "extern gsl::czstring toString(${WRAP_ENUM_NAME} val);\n\n" )
-
-    # EnumUtil forward declaration
-    file( APPEND "${_wrap_enum_tmp}" "template<typename T> struct EnumUtil;\n\n" )
-    # EnumUtil specialization
-    file( APPEND "${_wrap_enum_tmp}" "template<> struct EnumUtil<${WRAP_ENUM_NAME}>\n" )
-    file( APPEND "${_wrap_enum_tmp}" "{\n" )
-
-    # fromString()
-    file( APPEND "${_wrap_enum_tmp}" "    static ${WRAP_ENUM_NAME} fromString(const std::string& value);\n" )
-
-    # all()
-    file( APPEND "${_wrap_enum_tmp}" "    static std::map<${WRAP_ENUM_NAME}, std::string> all();\n" )
-
-    # name()
-    file( APPEND "${_wrap_enum_tmp}" "    static const char* name()\n" )
-    file( APPEND "${_wrap_enum_tmp}" "    {\n" )
-    file( APPEND "${_wrap_enum_tmp}" "        return \"${WRAP_ENUM_NAME}\";\n" )
-    file( APPEND "${_wrap_enum_tmp}" "    }\n" )
-
-    file( APPEND "${_wrap_enum_tmp}" "};\n" )
-    file( APPEND "${_wrap_enum_tmp}" "} // namespace ${WRAP_ENUM_NAMESPACE}\n" )
-    file( RENAME ${_wrap_enum_tmp} ${WRAP_ENUM_DEST} )
-
-
-    get_filename_component( _include_name ${WRAP_ENUM_DEST} NAME )
-    file( WRITE "${_wrap_enum_tmp}" "" )
-    # headers
-    file( APPEND "${_wrap_enum_tmp}" "#include \"${_include_name}\"\n" )
-    file( APPEND "${_wrap_enum_tmp}" "\n" )
-    file( APPEND "${_wrap_enum_tmp}" "#include <cstdint>\n" )
-    file( APPEND "${_wrap_enum_tmp}" "#include <gsl/gsl-lite.hpp>\n" )
-    file( APPEND "${_wrap_enum_tmp}" "#include <map>\n" )
-    file( APPEND "${_wrap_enum_tmp}" "#include <stdexcept>\n" )
-    file( APPEND "${_wrap_enum_tmp}" "#include <string>\n" )
-    file( APPEND "${_wrap_enum_tmp}" "\n" )
-
-    file( APPEND "${_wrap_enum_tmp}" "namespace ${WRAP_ENUM_NAMESPACE}\n" )
-    file( APPEND "${_wrap_enum_tmp}" "{\n" )
-
-    # toString function
-    file( APPEND "${_wrap_enum_tmp}" "gsl::czstring toString(${WRAP_ENUM_NAME} val)\n" )
-    file( APPEND "${_wrap_enum_tmp}" "{\n" )
-    file( APPEND "${_wrap_enum_tmp}" "    switch(val)\n" )
-    file( APPEND "${_wrap_enum_tmp}" "    {\n" )
-    foreach( _val IN LISTS _vals )
-        # remove trailing whitespace/comments
-        string( REGEX REPLACE "//.*\$" "" _val "${_val}" )
-        string( REPLACE " " "" _val "${_val}" )
-        string( REPLACE "\t" "" _val "${_val}" )
-        if( _val STREQUAL "" )
-            continue()
-        endif()
-        # remove "= 123" part
-        string( REGEX REPLACE "=.+\$" "" _ename "${_val}" )
-        if( _ename STREQUAL "" )
-            continue()
-        endif()
-        # emit: case Enum::Value: return "Value";
-        file( APPEND "${_wrap_enum_tmp}" "        case ${WRAP_ENUM_NAME}::${_ename}: return \"${_ename}\";\n" )
-    endforeach()
-    file( APPEND "${_wrap_enum_tmp}" "        default: return nullptr;\n" )
-    file( APPEND "${_wrap_enum_tmp}" "    }\n" )
-    file( APPEND "${_wrap_enum_tmp}" "}\n\n" )
-
-    # fromString()
-    file( APPEND "${_wrap_enum_tmp}" "${WRAP_ENUM_NAME} EnumUtil<${WRAP_ENUM_NAME}>::fromString(const std::string& value)\n" )
-    file( APPEND "${_wrap_enum_tmp}" "{\n" )
-    foreach( _val IN LISTS _vals )
-        # remove trailing whitespace/comments
-        string( REGEX REPLACE "//.*\$" "" _val "${_val}" )
-        string( REPLACE " " "" _val "${_val}" )
-        string( REPLACE "\t" "" _val "${_val}" )
-        if( _val STREQUAL "" )
-            continue()
-        endif()
-        string( REGEX REPLACE "=.+\$" "" _ename "${_val}" )
-        if( _ename STREQUAL "" )
-            continue()
-        endif()
-        if( _eval STREQUAL "" )
-            continue()
-        endif()
-        # emit: if(value == "Value") return Enum::Value;
-        file( APPEND "${_wrap_enum_tmp}" "    if(value == \"${_ename}\") return ${WRAP_ENUM_NAME}::${_ename};\n" )
-    endforeach()
-    file( APPEND "${_wrap_enum_tmp}" "    throw std::domain_error(\"Value '\" + value + \"' invalid for enum '${WRAP_ENUM_NAME}'\");\n" )
-    file( APPEND "${_wrap_enum_tmp}" "}\n\n" )
-
-    # all()
-    file( APPEND "${_wrap_enum_tmp}" "std::map<${WRAP_ENUM_NAME}, std::string> EnumUtil<${WRAP_ENUM_NAME}>::all()\n" )
-    file( APPEND "${_wrap_enum_tmp}" "{\n" )
-    file( APPEND "${_wrap_enum_tmp}" "    return {\n" )
-    foreach( _val IN LISTS _vals )
-        # remove trailing whitespace/comments
-        string( REGEX REPLACE "//.*\$" "" _val "${_val}" )
-        string( REPLACE " " "" _val "${_val}" )
-        string( REPLACE "\t" "" _val "${_val}" )
-        if( _val STREQUAL "" )
-            continue()
-        endif()
-        string( REGEX REPLACE "=.+\$" "" _ename "${_val}" )
-        if( _ename STREQUAL "" )
-            continue()
-        endif()
-        if( _eval STREQUAL "" )
-            continue()
-        endif()
-        # emit: { enum, string },
-        file( APPEND "${_wrap_enum_tmp}" "        {${WRAP_ENUM_NAME}::${_ename}, \"${_ename}\"},\n" )
-    endforeach()
-    file( APPEND "${_wrap_enum_tmp}" "    };\n" )
-    file( APPEND "${_wrap_enum_tmp}" "}\n" )
-
-    file( APPEND "${_wrap_enum_tmp}" "} // namespace ${WRAP_ENUM_NAMESPACE}\n" )
-    file( RENAME ${_wrap_enum_tmp} ${WRAP_ENUM_DEST_IMPL} )
+    add_custom_command(
+            OUTPUT ${WRAP_ENUM_DEST} ${WRAP_ENUM_DEST_IMPL}
+            COMMAND
+            Python3::Interpreter
+            ${CMAKE_SOURCE_DIR}/cmake/scripts/wrap_enum.py
+            --namespace ${WRAP_ENUM_NAMESPACE}
+            --name ${WRAP_ENUM_NAME}
+            --type ${WRAP_ENUM_TYPE}
+            --source ${WRAP_ENUM_SOURCE}
+            --dest ${WRAP_ENUM_DEST}
+            --dest-impl ${WRAP_ENUM_DEST_IMPL}
+            --with-includes ${WRAP_ENUM_WITH_INCLUDES}
+            COMMENT "Generating enum files from ${WRAP_ENUM_SOURCE}"
+            DEPENDS ${WRAP_ENUM_SOURCE}
+            VERBATIM
+    )
 endfunction()
