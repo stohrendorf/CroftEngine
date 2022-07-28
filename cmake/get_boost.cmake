462c0314b1fcbec97baae8e97d4604cfb19a3629
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2022-07-28 08:54:54 +0200

    remove boost stacktrace and addr2line dependencies

--- cmake/get_boost.cmake
+++ cmake/get_boost.cmake
@@ -9,28 +9,6 @@ endif()
 
 find_package( Boost COMPONENTS system log log_setup locale REQUIRED )
 
-add_library(
-        Boost::stacktrace INTERFACE IMPORTED
-)
-
 if( UNIX )
     target_compile_definitions( Boost::log INTERFACE -DBOOST_LOG_DYN_LINK )
-    if( NOT CMAKE_ADDR2LINE )
-        message( WARNING "add2line not found, stacktraces will be without symbol resolution" )
-        target_link_libraries( Boost::stacktrace INTERFACE Boost::stacktrace_basic )
-    else()
-        find_package( Boost COMPONENTS stacktrace_addr2line REQUIRED )
-        target_link_libraries( Boost::stacktrace INTERFACE Boost::stacktrace_addr2line )
-        target_compile_definitions(
-                Boost::stacktrace
-                INTERFACE
-                -DBOOST_STACKTRACE_USE_ADDR2LINE
-                -DBOOST_STACKTRACE_ADDR2LINE_LOCATION=${CMAKE_ADDR2LINE}
-        )
-        target_link_libraries(
-                Boost::stacktrace
-                INTERFACE
-                dl
-        )
-    endif()
 endif()
