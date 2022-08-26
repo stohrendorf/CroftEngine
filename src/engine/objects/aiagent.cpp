88ff2113257091e89a81b36928c5724266d45436
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2022-08-26 08:38:49 +0200

    use real speed to check against collision radius

--- src/engine/objects/aiagent.cpp
+++ src/engine/objects/aiagent.cpp
@@ -128,9 +128,9 @@ bool AIAgent::animateCreature(const core::Angle& deltaRotationY, const core::Ang
   // from the sector boundaries if it detects a collision. As the max norm is always greater than or equal to the
   // euclidean norm, checking that the max norm of movement is less than or equal to the collision radius means that
   // the movement did not cause a wall glitch, assuming that the position before applying the movement was valid.
-  if(const auto speed = abs(m_skeleton->calculateFloorSpeed()); speed > m_collisionRadius / 1_frame)
+  if(abs(m_state.speed) > m_collisionRadius / 1_frame)
   {
-    BOOST_LOG_TRIVIAL(warning) << m_skeleton->getName() << " movement speed of " << speed
+    BOOST_LOG_TRIVIAL(warning) << m_skeleton->getName() << " movement speed of " << m_state.speed
                                << " exceeds collision radius of " << m_collisionRadius;
   }
 
