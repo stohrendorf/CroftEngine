3a158b68c6f9ec7089fcb2be2d953eef7b385c7f
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2022-09-30 00:05:11 +0200

    bump cimg

--- cmake/get_cimg.cmake
+++ cmake/get_cimg.cmake
@@ -4,7 +4,7 @@ endif()
 
 include( ./dl_unpack )
 
-set( _cimg_version "v.3.1.2" )
+set( _cimg_version "v.3.1.6" )
 dl_unpack(
         URL https://github.com/dtschump/CImg/archive/${_cimg_version}.tar.gz
         FILENAME CImg-${_cimg_version}.tar.gz
