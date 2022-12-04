348b1d3d4ca4c0dd48dc01a1e4a9bc6bae91fc18
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2022-12-04 05:42:47 +0100

    bump dependencies and gamecontrollerdb

--- cmake/get_pybind11.cmake
+++ cmake/get_pybind11.cmake
@@ -4,7 +4,7 @@ endif()
 
 include( ./dl_unpack )
 
-set( _pybind11_version "2.10.0" )
+set( _pybind11_version "2.10.1" )
 dl_unpack(
         URL https://github.com/pybind/pybind11/archive/v${_pybind11_version}.tar.gz
         FILENAME pybind11-v${_pybind11_version}.tar.gz
