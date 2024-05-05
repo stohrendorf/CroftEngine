25f1443c772f96cb8f35422554347d011f5376c7
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2024-05-05 21:06:32 +0200

    use trivial maps for trivial keys

--- src/serialization/map.h
+++ src/serialization/map.h
@@ -4,25 +4,80 @@
 
 #include <map>
 #include <ryml.hpp>
+#include <type_traits>
 
 namespace serialization
 {
+// TODO this works only for very trivial types, but not types that are essentially trivial but are tagged, like IDs or references
+template<typename T>
+inline constexpr bool isTrivialKey
+  = std::is_integral_v<T> || std::is_floating_point_v<T> || std::is_enum_v<T> || std::is_same_v<T, std::string>;
+
 template<typename T, typename U, typename TContext>
 void serialize(const std::map<T, U>& data, const Serializer<TContext>& ser)
 {
-  ser.node |= ryml::SEQ;
-  ser.tag("map");
-  for(auto& [key, value] : data)
+  if constexpr(isTrivialKey<T>)
   {
-    const auto tmp = ser.newChild();
-    access<T, false>::dispatch(key, tmp["key"]);
-    access<U, false>::dispatch(value, tmp["value"]);
+    ser.node |= ryml::MAP;
+    for(auto& [key, value] : data)
+    {
+      const auto tmp = ser.newChild();
+      if constexpr(std::is_enum_v<T>)
+      {
+        auto enumValue = static_cast<std::underlying_type_t<T>>(key);
+        tmp.node << ryml::key(enumValue);
+      }
+      else
+      {
+        tmp.node << ryml::key(key);
+      }
+      access<U, false>::dispatch(value, tmp);
+    }
+  }
+  else
+  {
+    ser.node |= ryml::SEQ;
+    ser.tag("map");
+    for(auto& [key, value] : data)
+    {
+      const auto tmp = ser.newChild();
+      access<T, false>::dispatch(key, tmp["key"]);
+      access<U, false>::dispatch(value, tmp["value"]);
+    }
   }
 }
 
 template<typename T, typename U, typename TContext>
 void deserialize(std::map<T, U>& data, const Deserializer<TContext>& ser)
 {
+  if constexpr(isTrivialKey<T>)
+  {
+    if(ser.node.is_map())
+    {
+      gsl_Assert(ser.node.is_map());
+      data = std::map<T, U>();
+      for(const auto& element : ser.node.children())
+      {
+        gsl_Assert(element.has_key());
+
+        T key{};
+        if constexpr(std::is_enum_v<T>)
+        {
+          std::underlying_type_t<T> enumValue;
+          element >> ryml::key(enumValue);
+          key = T{enumValue};
+        }
+        else
+        {
+          element >> ryml::key(key);
+        }
+        data.emplace(key, access<U, true>::dispatch(ser.withNode(element)));
+      }
+      return;
+    }
+  }
+
+  gsl_Assert(ser.node.is_seq());
   ser.tag("map");
   data = std::map<T, U>();
   for(const auto& element : ser.node.children())
