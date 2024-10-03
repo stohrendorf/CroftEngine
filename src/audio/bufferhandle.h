b3a92db427fd936d9cb915a380a275508c3eca93
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2024-10-03 06:51:08 +0200

    fix bufferhandle duration calculation

--- src/audio/bufferhandle.h
+++ src/audio/bufferhandle.h
@@ -22,8 +22,8 @@ public:
 
   [[nodiscard]] Clock::duration getDuration() const noexcept
   {
-    return Clock::duration((m_sampleRate * Clock::duration::period::den)
-                           / (m_frameCount * Clock::duration::period::num));
+    return Clock::duration((m_frameCount * Clock::duration::period::den)
+                           / (m_sampleRate * Clock::duration::period::num));
   }
 
 private:
